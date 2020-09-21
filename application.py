1a75cce29b6bf572506cc4f6c42fd451b199c665
Michael Jones <32649229+moj124@users.noreply.github.com>
2020-09-21 21:40:54 +0100

    Add files via upload

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 import datetime
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey, Text, DateTime
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for, send_from_directory
-from flask_session import Session
+from flask_session.__init__ import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
